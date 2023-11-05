09d8c87555b6990e0231e030abb666cf58695c8e
Amiri <Amirymax@gmail.com>
2023-11-05 19:16:28 +0300

    Дарси 2

--- Шиносоӣ бо JavaScript/app.js
+++ Шиносоӣ бо JavaScript/app.js
@@ -1,1 +1,36 @@
-alert('Hello worldddddd!');
+// комменти якхата
+
+/*
+    комменти бисёрхата
+*/
+
+/**
+ * JSDoc коммент
+ */
+
+/**
+ * Намудҳои додаҳо:
+ * 1. Додаҳои ибтидоӣ ё примитивӣ
+ * - Number: 209, 1.5, NaN, Infitity
+ * - String: 'Hello', "Hello", `Hello`
+ * - Boolean: true, false
+ * - Null: null
+ * - Undefined: undefined
+ * - Symbol()
+ */
+console.log(2019, 1.5);
+console.log("Hello", 'Hello', `Hello`);
+console.log(true, false);
+console.log(null);
+console.log(undefined);
+console.log(Symbol());
+/**
+ * 2. Объектҳо. (Reference type) Бо ссылка дода мешаванд.
+ * - Object: {name: 'Max', age: 30}
+ *  * Array: [1,2,3]
+ *  * Function: function foo() {}
+ *  * Date: new Date()
+ * ...
+ */
+console.table({name: 'Max', age: 20});
+console.log([1,2,3,4]);
