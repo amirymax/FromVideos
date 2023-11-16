3415ca73e4047eae7972fc89e4d5d39de8c45939
Amiri <Amirymax@gmail.com>
2023-11-16 19:37:08 +0300

    Дарси 3

---
+++ Шиносоӣ бо JavaScript/Дарси 3/app.js
@@ -0,0 +1,39 @@
+//var, let, const
+
+var nom = 'Amiri';
+console.log(nom);
+
+nom = 'Ali';
+console.log(nom);
+
+var nasab;
+console.log(nasab);
+
+nasab = 'Ahmadzoda';
+console.log(nasab);
+
+var nasab = 'Shokirzoda';
+console.log(nasab);
+
+console.clear()
+
+let me = 'Ahmad';
+console.log(me);
+
+me = 'Muhammad';
+console.log(me);
+
+console.clear();
+
+const manager = 'Azim';
+console.log(manager);
+
+const user = {
+    nom: 'Alijon',
+    sol: 18
+}
+
+console.table(user);
+
+user.sol = 20;
+console.table(user);
